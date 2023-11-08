3904fd613ade0a603eaf949d251090d48f85018f
borjisabri <sabri.borji@bearstudio.fr>
2023-11-08 20:02:05 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+let modal = null;
+const focusableSelector = "button,a,input,textarea";
+let focusables = [];
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal = document.querySelector(e.target.getAttribute("href"));
+  focusables = Array.from(modal.querySelectorAll(focusableSelector));
+  modal.style.display = null;
+  modal.removeAttribute("aria-hidden");
+  modal.setAttribute("aria-modal", "true");
+  modal.addEventListener("click", closeModal);
+  modal.querySelector(".js-modal-close").addEventListener("click", closeModal);
+  modal
+    .querySelector(".js-modal-stop")
+    .addEventListener("click", stopPropagation);
+};
+const closeModal = function (e) {
+  if (modal === null) return;
+  e.preventDefault();
+  modal.style.display = "none";
+  modal.setAttribute("aria-hidden", "true");
+  modal.removeAttribute("aria-modal", "true");
+  modal.removeEventListener("click", closeModal);
+  modal
+    .querySelector(".js-modal-close")
+    .removeEventListener("click", closeModal);
+  modal
+    .querySelector(".js-modal-stop")
+    .removeEventListener("click", stopPropagation);
+
+  modal = null;
+};
+const focusInmodal = function (e) {
+  e.preventDefault();
+  let index = focus.findIndex((f) => f === modal.querySelector(":focus"));
+  index++;
+  if (index >= focusables.length) {
+    index = 0;
+  }
+  focusables[index].focus();
+};
+const stopPropagation = function (e) {
+  e.stopPropagation();
+};
+
+document.querySelectorAll(".js-modal").forEach((a) => {
+  a.addEventListener("click", openModal);
+});
+window.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" || e.key === "Esc") {
+    closeModal(e);
+  }
+  if (e.key === "Tab" && modal !== null) {
+    focusInmodal(e);
+  }
+});
